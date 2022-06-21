fda950b4cd3d1acf5129ebe1b7a00abd4c450f7d
Lol-ice <lolice@mail.ru>
2022-06-21 16:25:40 +0300

    1 commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    println(calculate())
+
+}
+
+fun calculate(amount: Double = 1000.00, commission: Double = 0.075, minCommission: Double = 35.00): String {
+
+    val finalCommission = if (amount * commission < minCommission) { minCommission
+    } else { amount * commission }
+    val finalSum = amount - finalCommission
+
+    return "При переводе: $amount \nКомиссия составит: $finalCommission \nСумма к зачислению: $finalSum"
+
+}
---
+++ src/test/kotlin/MainKtTest.kt
@@ -0,0 +1,49 @@
+import org.junit.Assert.*
+import org.junit.Test
+
+class MainKtTest {
+
+    @Test
+    fun calculate_test_commission() {
+
+        val amountT = 2000.00
+        val commissionT = 0.075
+        val minCommissionT = 35.00
+
+        val result = calculate(
+            amount = amountT,
+            commission = commissionT,
+            minCommission = minCommissionT,
+        )
+
+        assertEquals("При переводе: 2000.0 \nКомиссия составит: 150.0 \nСумма к зачислению: 1850.0", result)
+
+    }
+
+    @Test
+    fun calculate_test_min_commission() {
+
+        val amountT = 100.00
+        val commissionT = 0.075
+        val minCommissionT = 35.00
+
+        val result = calculate(
+            amount = amountT,
+            commission = commissionT,
+            minCommission = minCommissionT,
+        )
+
+        assertEquals("При переводе: 100.0 \nКомиссия составит: 35.0 \nСумма к зачислению: 65.0", result)
+
+    }
+
+    @Test
+    fun main_test() {
+
+        val result = main()
+
+        assertEquals(println(calculate()), result)
+
+    }
+
+}
